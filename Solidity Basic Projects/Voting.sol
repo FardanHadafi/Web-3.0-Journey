a8b9bdb0d52b59c7ec0858530ff31f1392f36f95
FardanHadafi <fardan.hadafi@yahoo.com>
2025-04-06 22:31:38 +0700

    Solidity Enhancement Day 4 ( Finishing Voting Project )

--- Solidity Basic Projects/Voting.sol
+++ Solidity Basic Projects/Voting.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 contract Voting {
     enum VoteStates {Absent, Yes, No} // Enum for voters change their choice
+    uint constant MAX_PROPOSALS = 10;
 
     struct Proposal {
         address target;
@@ -10,18 +11,33 @@ contract Voting {
         uint yesCount;
         uint noCount;
         mapping(address => VoteStates) voteState;
+        bool executed;
     }
 
     Proposal[] public proposals;
 
+    event ProposalCreated(uint);
+    event CastVote(uint, address indexed);
+
+    mapping(address => bool) members;
+
+    constructor(address[] memory _members) {
+        for(uint i; i < _members.length; i++) {
+            members[_members[i]] = true;
+        }
+        members[msg.sender] = true;
+    }
+
     function newProposal(address _target, bytes calldata _data) external { // Create a new proposal
+    require(members[msg.sender], "You are not a member");
+    emit ProposalCreated(proposals.length);
        Proposal storage proposal = proposals.push();
        proposal.target = _target;
        proposal.data = _data;
-
     }
 
     function castVote(uint256 proposalIndex, bool support) external { // Cast vote by inputed boolean value true / false
+    require(members[msg.sender], "You are not a member");
         Proposal storage proposal = proposals[proposalIndex];
 
         // clear out previous vote
@@ -42,5 +58,12 @@ contract Voting {
         // we're tracking whether or not someone has already voted 
         // and we're keeping track as well of what they voted
         proposal.voteState[msg.sender] = support ? VoteStates.Yes : VoteStates.No;
+        
+        emit CastVote(proposalIndex, msg.sender);
+
+        if(proposal.yesCount == MAX_PROPOSALS && !proposal.executed) {
+            (bool success, ) = proposal.target.call(proposal.data);
+            require(success);
+        }
     }
 }
